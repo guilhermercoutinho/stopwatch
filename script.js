b971765e1ce6a8f45e33ca7e4bab05eb659ff0aa
guilhermercoutinho <guilhermecoutinho.rc@gmail.com>
2025-01-28 22:12:05 -0300

    Alteração do botão inciar

--- script.js
+++ script.js
@@ -1,74 +1,84 @@
-const startButton = document.querySelector('.start')
-const stoptButton = document.querySelector('.stop')
-const resettButton = document.querySelector('.reset')
+const startStopBtn = document.querySelector(".start-stop");
+const stoptButton = document.querySelector(".stop");
+const resettButton = document.querySelector(".reset");
 
-const milli = document.querySelector('.milliseconds')
-const sec = document.querySelector('.seconds')
-const min = document.querySelector('.minutes')
+const txtMilliseconds = document.querySelector(".milliseconds");
+const txtSeconds = document.querySelector(".seconds");
+const txtMinutes = document.querySelector(".minutes");
 
-let milliNum = 0
-let secNum = 0
-let minNum = 0
-let intervalo
+let milliNum = 0;
+let secNum = 0;
+let minNum = 0;
+let intervalo;
+
+let isRunning = true;
 
 function milliseconds() {
-    milliNum++
-    if (milliNum < 10) {
-        milli.innerHTML = '00' + milliNum
-    } else if (milliNum >= 10 || milliNum <= 99){
-        milli.innerHTML = '0' + milliNum
-    } else {
-        milli.innerHTML = milliNum
-    }
+  milliNum++;
+  if (milliNum < 10) {
+    txtMilliseconds.innerHTML = "00" + milliNum;
+  } else if (milliNum >= 10 || milliNum <= 99) {
+    txtMilliseconds.innerHTML = "0" + milliNum;
+  } else {
+    txtMilliseconds.innerHTML = milliNum;
+  }
 
-    if (milliNum === 99) {
-        milliNum = 0
-        seconds()
-    }
+  if (milliNum === 99) {
+    milliNum = 0;
+    seconds();
+  }
 }
 
 function seconds() {
-    secNum++
-    if (secNum < 10) {
-        sec.innerHTML = '0' + secNum
-    } else {
-        sec.innerHTML = secNum
-    }
+  secNum++;
+  if (secNum < 10) {
+    txtSeconds.innerHTML = "0" + secNum;
+  } else {
+    txtSeconds.innerHTML = secNum;
+  }
 
-    if (secNum === 59) {
-        secNum = 0
-        minutes()
-    }
+  if (secNum === 59) {
+    secNum = 0;
+    minutes();
+  }
 }
 
 function minutes() {
-    minNum++
-    if (minNum < 10) {
-        min.innerHTML = '0' + minNum
-    } else {
-        min.innerHTML = minNum
-    }
+  minNum++;
+  if (minNum < 10) {
+    txtMinutes.innerHTML = "0" + minNum;
+  } else {
+    txtMinutes.innerHTML = minNum;
+  }
 }
 
 function start() {
-    clearInterval(intervalo)
+  isRunning = !isRunning;
+
+  if (isRunning) {
+    clearInterval(intervalo);
+    startStopBtn.textContent = "INICIAR";
+  } else {
     intervalo = setInterval(() => {
-        milliseconds()
+      milliseconds();
     }, 10);
+    startStopBtn.textContent = "PARAR";
+  }
 }
 
-const stop = () => clearInterval(intervalo)
+const stop = () => clearInterval(intervalo);
 
 function reset() {
-    clearInterval(intervalo)
-    milliNum = 0
-    secNum = 0
-    minNum = 0
-    milli.innerHTML = '000'
-    sec.innerHTML = '00'
-    min.innerHTML = '00'
-  }
+  clearInterval(intervalo);
+  milliNum = 0;
+  secNum = 0;
+  minNum = 0;
+  txtMilliseconds.innerHTML = "000";
+  txtSeconds.innerHTML = "00";
+  txtMinutes.innerHTML = "00";
+  startStopBtn.textContent = "INICIAR";
+}
 
-startButton.addEventListener("click", start)
-stoptButton.addEventListener("click", stop)
-resettButton.addEventListener("click", reset)
+startStopBtn.addEventListener("click", start);
+stoptButton.addEventListener("click", stop);
+resettButton.addEventListener("click", reset);
